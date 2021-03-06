a52edafb042fd5d37daf6d9ec0d5a294dac6c89f
Davi Nicollas <davi.nicollas@hotmail.com>
2021-03-06 09:34:10 -0300

    Manipulando arquivo

--- ex182/src/main/java/application/Program.java
+++ ex182/src/main/java/application/Program.java
@@ -6,8 +6,10 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -18,16 +20,15 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        String path = "C:\\temp\\ArquivoTest.txt";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        String[] line = new String[]{"Test", "Test"};
+        String path = "c:\\temp\\out.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))) {
+            for (String lines : line) {
+                bw.write(lines);
+                bw.newLine();
             }
         } catch (IOException e) {
-            System.out.println("ERRO: " + e.getMessage());
-        }
+            e.printStackTrace();
         }
     }
+}
