97e55ab5d2ef38f60a93dffea50f4b98705f8966
Davi Nicollas <davi.nicollas@hotmail.com>
2021-03-06 09:22:33 -0300

    att2

--- ex182/src/main/java/application/Program.java
+++ ex182/src/main/java/application/Program.java
@@ -5,7 +5,9 @@
  */
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -14,22 +16,31 @@ import java.util.Scanner;
  * @author davi nicollas
  */
 public class Program {
-    public static void main(String[] args){
-        File file = new  File("C:\\temp\\ArquivoTest.txt");
-        Scanner sc = null;
-        try{
-            sc = new Scanner(file);
-            while(sc.hasNextLine()){
-                System.out.println((sc.nextLine()));
+
+    public static void main(String[] args) {
+        String path = "C:\\temp\\ArquivoTest.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
-       
-        }
-         catch(IOException e){
-           System.out.println("ERRO: " + e.getMessage());     
-           }
-        finally{
-            if(sc !=null){
-                sc.close();
+        } catch (IOException e) {
+            System.out.println("ERRO: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
