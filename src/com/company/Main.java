a8f110714a9d55f7a31ae7a5cfdbc8d7ace93ec3
davinicollas <davi.nicollas@hotmail.com>
2021-03-10 10:54:31 -0300

    metodos Set

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<Integer> a = new TreeSet(Arrays.asList(0,2,4,5,6,8,10));
+        Set <Integer> b = new TreeSet(Arrays.asList(5,6,7,8,9,10));
+
+
+        // UNIÃO
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+
+        // INTERSEÇÃO
+        Set<Integer> d = new TreeSet<>(a);
+        c.retainAll(b);
+        System.out.println(d);
+
+        //DIFEREÇA
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
+
+    }
+}
