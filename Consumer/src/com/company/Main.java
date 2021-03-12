078bac0b49bded8a54d80a35c88afa77ecba3a5d
davinicollas <davi.nicollas@hotmail.com>
2021-03-12 09:26:03 -0300

    Consumer com função de att preço

---
+++ Consumer/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD case", 80.90));
+
+        list.forEach(new PriceUpdate());
+        list.forEach(System.out::println);
+    }
+}
