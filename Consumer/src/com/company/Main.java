d61a92c846df418f22a34f43f53053069afb6104
davinicollas <davi.nicollas@hotmail.com>
2021-03-12 09:43:08 -0300

    Referença method com expressao lambda declarada

--- Consumer/src/com/company/Main.java
+++ Consumer/src/com/company/Main.java
@@ -6,6 +6,7 @@ import util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Main {
 
@@ -18,8 +19,11 @@ public class Main {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD case", 80.90));
-
-        list.forEach(Product::nonStaticPriceUpdate);
+        double factor = 1.1;
+        Consumer<Product> cons = product -> {
+            product.setPrice(product.getPrice()*factor);
+        };
+        list.forEach(cons);
         list.forEach(System.out::println);
     }
 }
