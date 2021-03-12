cd081a48aaa3060d438d0cd97440b79f86f08e6c
davinicollas <davi.nicollas@hotmail.com>
2021-03-12 09:28:26 -0300

    Referença method com metodo estatico

--- Consumer/src/entities/Product.java
+++ Consumer/src/entities/Product.java
@@ -24,11 +24,9 @@ public class Product {
     public void setPrice(Double price) {
         this.price = price;
     }
-    public static boolean staticProductPredicate(Product product){
-        return product.getPrice() >= 100.00;
-    }
-    public boolean nonStaticProductPredicate(){
-        return price >= 100.00;
+
+    public static void staticPriceUpdate(Product p){
+        p.setPrice(p.getPrice()*1.1);
     }
 
     @Override
